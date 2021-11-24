d69616736f8118c4772925fe83ab11e126ec8931
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2021-11-24 07:44:21 +0100

    change clinet mongo

--- backend/srv.py
+++ backend/srv.py
@@ -6,6 +6,7 @@ import flask as fl
 import datetime
 from flask_cors import CORS
 from flask_mongoengine import MongoEngine
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from bson import json_util
 import json
@@ -14,43 +15,17 @@ PATH_TO_SAVE = "data/"
 PATH_T0_CSV = ""
 
 app = Flask(__name__, static_folder='../build/', static_url_path='/', )
+
+
+
+MONGO_URI = "mongodb://"+os.environ["MONGODB_USERNAME"]+":"+os.environ["MONGODB_PASSWORD"]+"@"+os.environ["MONGODB_HOST"]+":"+os.environ["MONGODB_PORT"]+"/"+os.environ["MONGODB_DB"]+"?authSource=admin"
+app.config["MONGO_URI"] = MONGO_URI
+
+mongo_client = PyMongo(app)
+db = mongo_client.db
+
 CORS(app)
-
-
-#app.config["MONGO_URI"] = "mongodb://localhost:27017/"+os.environ['MONGODB_DB']
-app.config['MONGODB_SETTINGS'] = {
-    'host': os.environ['MONGODB_HOST'],
-    'port': int(os.environ['MONGODB_PORT']),
-    
-    'db': os.environ['MONGODB_DB']
-}
-
-db = MongoEngine()
-db.init_app(app)
-class Test(db.Document):
-    test_texte = db.StringField(max_length=100)
-    test_texte2 = db.StringField(max_length=100)
-    
-    def get_id(self):
-        return json.dumps(self.id, default=str)
-class UserInformations(db.DynamicDocument):
-    initialValues = db.StringField(max_length=60)
-    age = db.StringField()
-    gender = db.BooleanField(default=False)
-    studieLevel = db.DateTimeField(default=datetime.datetime.now)
-    studieArea = db.StringField()
-    fatigueLevel = db.StringField()
-    armyLengthOfService = db.StringField()
-    typeOfJob = db.StringField()
-    peopleCommand = db.StringField()
-    headquarters = db.StringField()
-    jobLengthOfService = db.StringField()
-    grade = db.StringField()
-    date = db.StringField()
-    
-    def get_id(self):
-        return json.dumps(self.id, default=str)
-    
+ 
 @app.route('/')
 def index():
     if os.environ['FLASK_ENV'] == "production":
@@ -67,10 +42,16 @@ def put_personal_information():
         response.status_code=204
         return response
     else:
+        _id = db.user_information.insert(data)
+        response.status_code=200  
+        response.data=str(_id)
+        return response
+        """
         userInformations = UserInformations(data)
         userInformations.save()
         response.status_code = 200  
         response.data = userInformations.get_id()
+        """
         return response
     
 
@@ -84,8 +65,7 @@ def put_personal_evaluation():
         response.status_code=204
         return response
     else:
-        userInformations_to_update = UserInformations.objects(id=ObjectId(data.pop("_id")))
-        userInformations_to_update.update(**data)
+        db.user_information.find_one_and_update({"_id" :ObjectId(data.pop("_id"))},{"$set":data})
         response.status_code=200  
         return response
     
@@ -101,18 +81,21 @@ def put_personal_rate():
     
         response
     else:
-        userInformations_to_update = UserInformations.objects(id=ObjectId(data.pop("_id")))
-        userInformations_to_update.update(**data)
+        db.user_information.find_one_and_update({"_id" :ObjectId(data.pop("_id"))},{"$set":data})
         response.status_code=200
         return response
     
 
 def check_form(data):
-    for element in data.values():
-        if type(element) == type(None):
-            return None
-        if element == "":   
-            return None
+    print(data)
+    if data is not None:
+        for element in data.values():
+            if type(element) == type(None):
+                return None
+            if element == "":   
+                return None
+    else:
+        return None
     return data
 
 def generate_dataset():
