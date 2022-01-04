55c19ce6745ee431dc7f3e3151a814e9874cb60c
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2022-01-04 17:27:41 +0100

    change dockerfile

--- backend/srv.py
+++ backend/srv.py
@@ -15,7 +15,7 @@ import json
 PATH_TO_SAVE = "data/"
 PATH_T0_CSV = ""
 
-app = Flask(__name__, static_folder='../frontend/build/', static_url_path='/', )
+app = Flask(__name__, static_folder='frontend/build/', static_url_path='/', )
 
 
 
