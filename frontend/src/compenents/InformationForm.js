37fbba12280e123f22dbc1e8978b44b9bf1ddfe8
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2022-02-22 16:42:15 +0100

    udpate section 1

--- frontend/src/compenents/InformationForm.js
+++ frontend/src/compenents/InformationForm.js
@@ -127,7 +127,7 @@ class InformationForm extends React.Component {
                 fontSize: "1.50rem",
               }}
             >
-              Section 1 : Information personnelle dsfsd
+              Section 1 : Information personnelle
             </Card.Title>
               <Form
                 style={{
