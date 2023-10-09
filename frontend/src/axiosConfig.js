0515f408fb0006e29b08e77621297ca783367d18
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2023-10-09 10:14:46 +0000

    add sequence order

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: process.env.REACT_APP_FLASK_URL || "http://localhost:5000/",
+ baseURL: "http://192.168.1.2:5000/",
 });
 
 export default api;
